1114732034edaa8f7d2893852af9bd641995e753
lizhoucheng1997@gmail.com <lizhoucheng@tamu.edu>
2019-07-21 23:11:56 -0500

    UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonModalComponent } from './person-modal/person-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/person-modal/person-modal.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-person-modal',
+  templateUrl: './person-modal.component.html',
+  styleUrls: ['./person-modal.component.scss'],
+  animations: [
+    trigger('dialog', [
+      transition('void => *', [
+        style({ transform: 'scale3d(.3, .3, .3)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale3d(.0, .0, .0)' }))
+      ])
+    ])
+  ]
+})
+export class PersonModalComponent implements OnInit {
+  @Input() closable = true;
+  @Input() visible: boolean;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+}
